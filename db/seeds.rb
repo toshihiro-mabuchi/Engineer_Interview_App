4e6af49f418c92c3d6192cc398f439f0cd23ef80
toshihiro-mabuchi <toshihiro_mabuchi@yahoo.co.jp>
2021-10-07 14:15:52 +0900

    patternモデル作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,16 @@
+
+# 管理者データー作成
 User.create!(
   email: "admin@email.com",
   password: "password",
   password_confirmation: "password"
 )
+
+# 面談パターンサンプルデータ作成
+4.times do |i|
+  n = i + 1 
+  Pattern.create!(
+    name: "パターン#{n}"
+)
+
+end
