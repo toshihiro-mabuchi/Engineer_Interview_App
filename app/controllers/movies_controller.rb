e8793a3d52da6b33cffe14bcc34bd9ed8e77d230
toshihiro-mabuchi <toshihiro_mabuchi@yahoo.co.jp>
2021-10-10 22:27:00 +0900

    アクション追加 & seedファイル変更

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,33 @@
 class MoviesController < ApplicationController
+
+  # GET /resource/index
+  def index
+
+  end
+
+  # GET /resource/new
+  def new
+
+  end
+
+  # POST /resource
+  def create
+
+  end
+
+  # GET /resource/edit
+  def edit
+
+  end
+
+  # PUT /resource
+  def update
+
+  end
+
+  # DELETE /resource
+  def destroy
+
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,43 +15,83 @@ User.create!(
 
 # 面談動画サンプルデーター作成
 Movie.create!(
+  title: "【WEB面接（スマホ/PC）】 ポイント（実例/練習用）",
+  thumbnail_url: "https://i.ytimg.com/vi/MrUHouwR43o/hqdefault.jpg",
+  youtube_mid: "MrUHouwR43o",
+  youtube_url: "https://youtu.be/MrUHouwR43o",
+  author_name: "就活レシピ",
+  pattern_id: 1,
   order_number: 1,
-  title: "賛美歌３１２番（いつくしみ深き）",
-  thumbnail_url: "https://i.ytimg.com/vi/Bo-Cmd3JM4c/hqdefault.jpg",
-  youtube_mid: "Bo-Cmd3JM4c",
-  youtube_url: "https://youtu.be/Bo-Cmd3JM4c",
-  author_name: "backbone777",
-  pattern_id: 1,
   display_flag: true)
 
 Movie.create!(
+  title: "【完全版】WEB面接の通過率が劇的に上がる25のチェックリスト【就活/就職/転職】",
+  thumbnail_url: "https://i.ytimg.com/vi/RyODmVaoQZ0/hqdefault.jpg",
+  youtube_mid: "RyODmVaoQZ0",
+  youtube_url: "https://youtu.be/RyODmVaoQZ0",
+  author_name: "ひろさんチャンネル【面接アドバイザー社長】",
+  pattern_id: 1,
   order_number: 2,
-  title: "【讃美歌320番】主よ御許に近づかん Nearer,My God,to Thee",
-  thumbnail_url: "https://i.ytimg.com/vi/VHLVDYQ9n5g/hqdefault.jpg",
-  youtube_mid: "VHLVDYQ9n5g",
-  youtube_url: "https://youtu.be/VHLVDYQ9n5g",
-  author_name: "S K",
-  pattern_id: 1,
   display_flag: true)
 
 Movie.create!(
+  title: "【ガチで内定が取れる面接練習法】就活で明日から使える8つの面接テクニックで内定獲得！",
+  thumbnail_url: "https://i.ytimg.com/vi/j8bDR_0bIoE/hqdefault.jpg",
+  youtube_mid: "j8bDR_0bIoE",
+  youtube_url: "https://youtu.be/j8bDR_0bIoEhttps://youtu.be/j8bDR_0bIoE",
+  author_name: "ひろさんチャンネル【面接アドバイザー社長】",
+  pattern_id: 1,
   order_number: 3,
-  title: "讃美歌461番「主われを愛す」",
-  thumbnail_url: "https://i.ytimg.com/vi/xJ0cJ5DQeIs/hqdefault.jpg",
-  youtube_mid: "xJ0cJ5DQeIs",
-  youtube_url: "https://youtu.be/xJ0cJ5DQeIs",
-  author_name: "meijigakuin",
-  pattern_id: 1,
   display_flag: true)
 
 Movie.create!(
-  order_number: 4,
-  title: "主の愛に生かされて",
-  thumbnail_url: "https://i.ytimg.com/vi/zI2GcNhvovg/hqdefault.jpg",
-  youtube_mid: "zI2GcNhvovg",
-  youtube_url: "https://youtu.be/zI2GcNhvovg",
-  author_name: "mickey71066",
+  title: "【断言】転職の面接は、この流れで進みます。",
+  thumbnail_url: "https://i.ytimg.com/vi/_4ejLOdJ_Ok/hqdefault.jpg",
+  youtube_mid: "_4ejLOdJ_Ok",
+  youtube_url: "https://youtu.be/_4ejLOdJ_Ok",
+  author_name: "【転職】サラタメのホワイト転職",
   pattern_id: 1,
+  order_number: 4,
   display_flag: true)
 
+Movie.create!(
+  title: "この逆質問を使って、面接を無双しちゃってください",
+  thumbnail_url: "https://i.ytimg.com/vi/S5RZmWHhFiQ/hqdefault.jpg",
+  youtube_mid: "S5RZmWHhFiQ",
+  youtube_url: "https://youtu.be/S5RZmWHhFiQ",
+  author_name: "末永 雄大 / すべらない転職エージェント",
+  pattern_id: 2,
+  order_number: 1,
+  display_flag: true)
+
+Movie.create!(
+  title: "【面接対策】転職の面接で合否を分けるたった2つのポイントを転職エージェントが解説！",
+  thumbnail_url: "https://i.ytimg.com/vi/hhEkZxRMIUc/hqdefault.jpg",
+  youtube_mid: "hhEkZxRMIUc",
+  youtube_url: "https://youtu.be/hhEkZxRMIUc",
+  author_name: "末永 雄大 / すべらない転職エージェント",
+  pattern_id: 2,
+  order_number: 2,
+  display_flag: true)
+
+Movie.create!(
+  title: "【面接通過率80%】未経験WEBエンジニアが面接で気をつけた3つのこと",
+  thumbnail_url: "https://i.ytimg.com/vi/mEOq2H2WX-c/hqdefault.jpg",
+  youtube_mid: "mEOq2H2WX-c",
+  youtube_url: "https://youtu.be/mEOq2H2WX-c",
+  author_name: "かよちんchannel",
+  pattern_id: 2,
+  order_number: 3,
+  display_flag: true)
+
+Movie.create!(
+  title: "【90％の人が知らない】エンジニアの面接で答えられるようにしておくべき3つの質問",
+  thumbnail_url: "https://i.ytimg.com/vi/Pn7rTqSCS5c/hqdefault.jpg",
+  youtube_mid: "Pn7rTqSCS5c",
+  youtube_url: "https://youtu.be/Pn7rTqSCS5c",
+  author_name: "エンジニアチャンネル",
+  pattern_id: 2,
+  order_number: 4,
+  display_flag: true)
+  
 end
