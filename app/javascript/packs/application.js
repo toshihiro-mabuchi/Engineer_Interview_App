6539b5769f8eef2151437752705595c45f0eeb5b
馬渕俊広 <toshihiro@mabuchitoshihironoMacBook-Pro.local>
2021-11-01 12:12:02 +0900

    bootstrap install

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,6 @@
+import 'bootstrap'
+import '../stylesheets/application'
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
@@ -5,3 +8,22 @@ require("channels")
 
 // jquery
 require('jquery')
+// require('bootstrap/dist/js/bootstrap.min.js')
+
+
+
+// import Rails from "@rails/ujs"
+// import Turbolinks from "turbolinks"
+// import * as ActiveStorage from "@rails/activestorage"
+// import "channels"
+// import 'bootstrap'
+// import '../src/application.scss'
+
+// require("jquery")
+
+// Rails.start()
+// Turbolinks.start()
+// ActiveStorage.start()
+
+// // モーダルが開かないので追記
+// window.jQuery = window.$ = require('jquery')
